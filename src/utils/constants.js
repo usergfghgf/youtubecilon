879c3c13de909f2343c0b2df781855128f1500b9
usergfghgf <instaff0192@gmail.com>
2025-06-07 18:04:29 +0530

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/1La4QzGeaaQ/hqdefault_480x360.jpg';
+export const demoVideoUrl = '/video/1La4QzGeaaQ';
+export const demoVideoTitle = 'Build and Deploy a Modern YouTube Clone Application in React JS';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png';
+
+export const categories = [
+  { name: 'New', icon: () => <span></span> },
+  { name: 'Coding', icon: () => <span></span> },
+  { name: 'News', icon: () => <span></span> },
+  { name: 'Beauty', icon: () => <span></span> },
+  { name: 'Music', icon: () => <span></span> },
+  { name: 'Education', icon: () => <span></span> },
+  { name: 'Podcast', icon: () => <span></span> },
+  { name: 'Gaming', icon: () => <span></span> },
+  { name: 'Motivational', icon: () => <span></span> },
+  
+];
---
+++ src/utils/formatDuration.js
@@ -0,0 +1,11 @@
+export const formatDuration = (isoDuration) => {
+  const match = isoDuration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+  const hours = parseInt(match[1]) || 0;
+  const minutes = parseInt(match[2]) || 0;
+  const seconds = parseInt(match[3]) || 0;
+
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+};
